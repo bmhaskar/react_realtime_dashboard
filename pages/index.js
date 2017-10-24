6c042739f82ee1e0067d144e5357549984542e45
Bharat Mhaskar <bharat.mhaskar@talentica.com>
2017-10-24 17:01:44 +0530

    Updated setup with style handling and added chart rendering library c3js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,53 @@
-export default () => (
-    <div>
-        <style jsx>{`
-      p {
-        color: red;
-      }
-    `}</style>
-        <p>Hello World!</p>
-    </div>
-)
+import stylesheet from './index.scss'
+
+import React, {Component} from "react";
+
+import C3Chart from 'react-c3js';
+import 'c3/c3.css';
+
+const data = {
+    data: {
+        x: 'x',
+        columns: [
+            ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
+            ['Battery',0 ,10, 20, 15,30],
+
+        ]
+    },
+    axis: {
+        x: {
+            type: 'timeseries',
+            tick: {
+                format: '%Y-%m-%d'
+            }
+        }
+    },
+    size: {
+        width: 640,
+        height:480
+
+    },
+    zoom: {
+        enabled: true
+    }
+};
+console.log(stylesheet);
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+    render() {
+        return (
+            <div>
+                <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+
+                <C3Chart  {...data} />
+            </div>
+        )
+    }
+}
+export default App;
--- server.js
+++ server.js
@@ -1,52 +1,35 @@
-const { createServer } = require('http');
-const url = require('url');
-
-const express = require('express');
-const WebSocket = require('ws');
-const next = require('next');
+const app = require('express')()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const next = require('next')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({dev})
-const handle = app.getRequestHandler()
+const nextApp = next({ dev })
+const nextHandler = nextApp.getRequestHandler()
 
-app.prepare()
-    .then(() => {
-        const app = express();
+// fake DB
+const messages = []
 
-        // app.get('/a', (req, res) => {
-        // return app.render(req, res, '/b', req.query)
-        // })
+// socket.io server
+io.on('connection', socket => {
+    socket.on('message', (data) => {
+        messages.push(data)
+        socket.broadcast.emit('message', data)
+    })
+})
 
-        // app.get('/b', (req, res) => {
-        //     return app.render(req, res, '/a', req.query)
-        // })
-        //
-        app.get('*', (req, res) => {
-            return handle(req, res)
-        })
+nextApp.prepare().then(() => {
+    app.get('/messages', (req, res) => {
+        res.json(messages)
+    })
 
+    app.get('*', (req, res) => {
+        return nextHandler(req, res)
+    })
 
-
-        const server = createServer(app);
-        const wss = new WebSocket.Server({ server });
-
-        wss.on('connection', function connection(ws, req) {
-            const location = url.parse(req.url, true);
-            // You might use location.query.access_token to authenticate or share sessions
-            // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
-
-            ws.on('message', function incoming(message) {
-                console.log('received: %s', message);
-            });
-
-            ws.send('something');
-        });
-
-        server.listen(port, (err) => {
-            if (err) throw err
-            console.log(`> Ready on http://localhost:${port}`)
-        })
-
-
-    });
+    server.listen(port, (err) => {
+        if (err) throw err
+        console.log(`> Ready on http://localhost:${port}`)
+    })
+})
